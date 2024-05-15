e043714d7880e7a1fe429752d73af3b61f73d35d
matias <you@example.com>
2024-05-15 16:14:26 -0300

    renamed Alert model to Alarms

--- database/migrations/2024_05_15_185451_create_alarms_table.php
+++ database/migrations/2024_05_15_185451_create_alarms_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('alert', function (Blueprint $table) {
+        Schema::create('alarms', function (Blueprint $table) {
             $table->id();
             $table->integer("treshold");
             $table->boolean("trigger_when_suprass");
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('alert');
+        Schema::dropIfExists('alarms');
     }
 };
